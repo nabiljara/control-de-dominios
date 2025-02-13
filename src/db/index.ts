97c7805e85a5e07e7ac1d6e244575c31b2bf687b
nabiljara <nabiljara26@gmail.com>
2025-02-13 11:32:09 -0300

    test singleton

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,19 +1,56 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
+// import { drizzle } from 'drizzle-orm/postgres-js';
 
-import postgres from 'postgres';
+// import postgres from 'postgres';
 
-// import { neon } from '@neondatabase/serverless';
+// import * as schema from './schema'
 
-import * as schema from './schema'
+// const DATABASE_URL = process.env.DATABASE_URL ?? "";
+
+// let sqlInstance: ReturnType<typeof postgres> | null = null;
+
+// function getSqlInstance() {
+//   if (!sqlInstance) {
+//     sqlInstance = postgres(DATABASE_URL, {
+//       max: 1,
+//       idle_timeout: 10
+//     });
+//   }
+//   return sqlInstance;
+// }
+
+// const sql = getSqlInstance();
+
+// // export const sql = postgres(
+// //   DATABASE_URL,
+// //   {
+// //     max: 5,
+// //     idle_timeout: 10
+// //   }
+// // );
+
+// const db = drizzle(sql, { schema });
+
+// export type DB = typeof db;
+
+// export default db;
+
+
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
 
 const DATABASE_URL = process.env.DATABASE_URL ?? "";
-export const sql = postgres(
-  DATABASE_URL
-);
-// export const sql = neon(DATABASE_URL);
 
+// Evita que se reinicialice la conexión en cada recarga en desarrollo
+const globalForDb = globalThis as unknown as { sql: ReturnType<typeof postgres> | undefined };
+
+if (!globalForDb.sql) {
+  console.log("📡 Creando conexión única a PostgreSQL...");
+  globalForDb.sql = postgres(DATABASE_URL);
+}
+
+export const sql = globalForDb.sql;
 const db = drizzle(sql, { schema });
 
 export type DB = typeof db;
-
 export default db;
