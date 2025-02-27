1a09fb04e196472d7925caff49275c42dcd9ca3b
nabiljara <nabiljara26@gmail.com>
2025-02-27 09:54:05 -0300

    test connection db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,35 +1,56 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
+// import { drizzle } from 'drizzle-orm/postgres-js';
 
-import postgres from 'postgres';
+// import postgres from 'postgres';
 
-import * as schema from './schema'
+// import * as schema from './schema'
+
+// const DATABASE_URL = process.env.DATABASE_URL ?? "";
+
+// let sqlInstance: ReturnType<typeof postgres> | null = null;
+
+// function getSqlInstance() {
+//   if (!sqlInstance) {
+//     sqlInstance = postgres(DATABASE_URL, {
+//       max: 5,
+//       idle_timeout: 10
+//     });
+//   }
+//   return sqlInstance;
+// }
+
+// const sql = getSqlInstance();
+
+// // export const sql = postgres(
+// //   DATABASE_URL,
+// //   {
+// //     max: 5,
+// //     idle_timeout: 10
+// //   }
+// // );
+
+// const db = drizzle(sql, { schema });
+
+// export type DB = typeof db;
+
+// export default db;
+
+
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
 
 const DATABASE_URL = process.env.DATABASE_URL ?? "";
 
-let sqlInstance: ReturnType<typeof postgres> | null = null;
+// Evita que se reinicialice la conexión en cada recarga en desarrollo
+const globalForDb = globalThis as unknown as { sql: ReturnType<typeof postgres> | undefined };
 
-function getSqlInstance() {
-  if (!sqlInstance) {
-    sqlInstance = postgres(DATABASE_URL, {
-      max: 5,
-      idle_timeout: 10
-    });
-  }
-  return sqlInstance;
+if (!globalForDb.sql) {
+  console.log("📡 Creando conexión única a PostgreSQL...");
+  globalForDb.sql = postgres(DATABASE_URL);
 }
 
-const sql = getSqlInstance();
-
-// export const sql = postgres(
-//   DATABASE_URL,
-//   {
-//     max: 5,
-//     idle_timeout: 10
-//   }
-// );
-
+export const sql = globalForDb.sql;
 const db = drizzle(sql, { schema });
 
 export type DB = typeof db;
-
 export default db;
