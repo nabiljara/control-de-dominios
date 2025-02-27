cc121be26174ed797294446ab23c302cb8786e30
luccamansilla <luccamansilla01@gmail.com>
2025-02-27 13:04:52 -0300

    change db conn

--- src/db/index.ts
+++ src/db/index.ts
@@ -33,22 +33,58 @@
 
 // export default db;
 
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import * as schema from "./schema";
+// import { drizzle } from "drizzle-orm/postgres-js";
+// import postgres from "postgres";
+// import * as schema from "./schema";
+
+// const DATABASE_URL = process.env.DATABASE_URL ?? "";
+
+// // Evita que se reinicialice la conexión en cada recarga en desarrollo
+// const globalForDb = globalThis as unknown as { sql: ReturnType<typeof postgres> | undefined };
+
+// if (!globalForDb.sql) {
+//   console.log("📡 Creando conexión única a PostgreSQL...");
+//   globalForDb.sql = postgres(DATABASE_URL);
+// }
+
+// export const sql = globalForDb.sql;
+// const db = drizzle(sql, { schema });
+
+// export type DB = typeof db;
+// export default db;
+
+import { drizzle } from 'drizzle-orm/postgres-js';
+
+import postgres from 'postgres';
+
+import * as schema from './schema'
 
 const DATABASE_URL = process.env.DATABASE_URL ?? "";
 
-// Evita que se reinicialice la conexión en cada recarga en desarrollo
-const globalForDb = globalThis as unknown as { sql: ReturnType<typeof postgres> | undefined };
+let sqlInstance: ReturnType<typeof postgres> | null = null;
 
-if (!globalForDb.sql) {
-  console.log("📡 Creando conexión única a PostgreSQL...");
-  globalForDb.sql = postgres(DATABASE_URL);
+function getSqlInstance() {
+  if (!sqlInstance) {
+    sqlInstance = postgres(DATABASE_URL, {
+      max: 5,
+      idle_timeout: 10
+    });
+  }
+  return sqlInstance;
 }
 
-export const sql = globalForDb.sql;
+const sql = getSqlInstance();
+
+// export const sql = postgres(
+//   DATABASE_URL,
+//   {
+//     max: 5,
+//     idle_timeout: 10
+//   }
+// );
+
 const db = drizzle(sql, { schema });
 
 export type DB = typeof db;
-export default db;
+
+export default db;
