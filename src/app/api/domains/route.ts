598ae3df572d252d83e9e998d88d4e3cc07fde6b
nabiljara <nabiljara26@gmail.com>
2025-03-10 14:05:32 -0300

    commented authorization for testing

--- src/app/api/domains/route.ts
+++ src/app/api/domains/route.ts
@@ -10,12 +10,12 @@ import { formatTextDate } from '@/lib/utils';
 
 export const runtime = 'nodejs';
 export async function GET(request: NextRequest) {
-    const authHeader = request.headers.get('authorization');
-    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-      return new Response('Unauthorized', {
-        status: 401,
-      });
-    }
+    // const authHeader = request.headers.get('authorization');
+    // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    //   return new Response('Unauthorized', {
+    //     status: 401,
+    //   });
+    // }
 
     const today = Date.now();
 
