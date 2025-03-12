f2ce1486d67df32487a09544b4e998aba272c354
nabiljara <nabiljara26@gmail.com>
2025-03-12 08:53:26 -0300

    remove authorization

--- src/app/api/notifications/route.ts
+++ src/app/api/notifications/route.ts
@@ -6,11 +6,6 @@ import { eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const authHeader = req.headers.get("authorization");
-  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
-
   try {
     const payload = await req.json();
 
