43a19624ec2b424dfe338ffe587f6ac9e485e93c
luccamansilla <luccamansilla01@gmail.com>
2025-03-13 09:34:24 -0300

    api cron shedule change and cron/domain require auth

--- src/app/api/cron/domains/route.ts
+++ src/app/api/cron/domains/route.ts
@@ -1,11 +1,18 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  try {
+export async function GET(req: Request) {
+  
+    const authHeader = req.headers.get("authorization");
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    try {
     const response = await fetch(`${process.env.BASE_URL}/api/domains`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        "Authorization": `Bearer ${process.env.CRON_SECRET}`
       },
     });
     const data = await response.json();
