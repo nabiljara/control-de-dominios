e0bdf8d8a56d2a14ccd237033cd000ddf1f868c3
luccamansilla <luccamansilla01@gmail.com>
2025-03-25 09:48:46 -0300

    seed notifications fix for deploy

--- src/db/seeds/index.ts
+++ src/db/seeds/index.ts
@@ -2,6 +2,5 @@ export { seed as clients } from "@/db/seeds/clients";
 export { seed as providers } from "@/db/seeds/providers";
 export { seed as localities } from "@/db/seeds/localities";
 export { seed as users } from "@/db/seeds/users";
-export { seed as notifications } from "@/db/seeds/notifications";
 export { seed as contacts } from "@/db/seeds/contacts";
 export { seed as domains } from "@/db/seeds/domains";
